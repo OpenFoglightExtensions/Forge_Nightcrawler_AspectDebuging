30eaf9494f20d0c46925d5c041679874ae7936c5
Stefan Marx <smarx@marxworld.net>
2013-06-23 23:40:46 +0200

    Test Class for external Breakpoints

---
+++ Aspects/src/java/de/dsg/forge/nc/aspectDebugger/Test.java
@@ -0,0 +1,14 @@
+package de.dsg.forge.nc.aspectDebugger;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: stefan
+ * Date: 23.06.13
+ * Time: 23:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void register() {
+        System.out.println("caught");
+    }
+}
